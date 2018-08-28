0d3adad63a164c2f3099a44f8bc403bfe0f3b14f
Cristian Gonzalez <31244463+You7Cris@users.noreply.github.com>
2018-08-28 11:34:40 -0500

    Add files via upload

---
+++ Proyectos Programacion Orientado a Objetos/TallerClase/src/Paquete1/Punto10.java
@@ -0,0 +1,14 @@
+package Paquete1;
+
+import java.util.Scanner;
+
+public class Punto10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner (System.in);
+		
+		
+	}
+
+}
